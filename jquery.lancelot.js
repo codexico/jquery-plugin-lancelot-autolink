d179246e72b88b33d2ff47cae98d6d09b2dfed5a
codexico <codexico@gmail.com>
2010-08-03 15:54:52 -0300

    demo com mouseover location

--- jquery.lancelot.js
+++ jquery.lancelot.js
@@ -1,8 +1,17 @@
 (function($){
- $.fn.truncate = function() {
+ $.fn.lancelot = function() {
 
     return this.each(function() {
+		//alert(this.text)
+	$(this).mouseover(function(){
+		alert('mouseover location')
+		window.location = this;
+		//alert('mouseover open _blank')
+		 //window.open(this, '_blank');
+		//alert('mouseover open')
+		 //window.open(this);
 
+	})
     });
  };
 })(jQuery);
