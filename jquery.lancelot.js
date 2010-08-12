4cb167f777abfb494d08eee2bbe47ad0a08a4de5
codexico <codexico@gmail.com>
2010-08-12 05:17:10 -0300

    display only by css

--- jquery.lancelot.js
+++ jquery.lancelot.js
@@ -1,72 +1,58 @@
 (function($){
     $.fn.lancelot = function(options) {
-
-        var defaults = {
+		var L = {};
+        L.defaults = {
             hoverTime: 2000,		//time to launch the link
             aclass: "lancelotGo",       //style, see lancelot.css
             atext: "",			//text to show
             show: "false",		//
-            display: "inline",		//inline, block ...
             speed: "fast",		//animation
             linkAction: "location",     //open, _blank
             atitle: "go in 2s",		//link title
             alink: false,		//url OR function
             element: "a"                //element to hover
         }
-        var options = $.extend(defaults, options);
+        L.o = $.extend(L.defaults, options);
 
         //the plugin
         return this.each(function() {
             obj = $(this);
-            var t = '';
 
             //where we go?
-            var ahref = "";
-            if(options.alink != false){
-                $.isFunction(options.alink) ?
-                ahref = options.alink.call()
-                : ahref = options.alink;
+            L.ahref = "";
+            if(L.o.alink != false){
+                $.isFunction(L.o.alink) ?
+                L.ahref = L.o.alink.call()
+                : L.ahref = L.o.alink;
             }else{
-                ahref = obj.attr("href");
+                L.ahref = obj.attr("href");
             }
 
             //create element
-            if(options.element !== "a"){
-                obj.append($( document.createElement(options.element)).addClass(options.aclass))
+            if(L.o.element !== "a"){
+                obj.append($( document.createElement(L.o.element)).addClass(L.o.aclass))
             }else{
-                obj.append(' <a href="'+ahref+'" class="'+options.aclass+'" title="'+options.atitle+'">'+options.atext+'</a>');
+                obj.append(' <a href="'+L.ahref+'" class="'+L.o.aclass+'" title="'+L.o.atitle+'">'+L.o.atext+'</a>');
             }
-            var goLink = obj.find("."+options.aclass).css("display", options.display);
+            L.goLink = obj.find("."+L.o.aclass);
 
 
             //show animation
-            if(options.show != "true"){
-                goLink.hide()
+            if(L.o.show != "true"){
+                L.goLink.hide()
                 obj.hover(
                     function(){
-                        goLink.fadeIn(options.speed)
+                        L.goLink.fadeIn(L.o.speed)
                     },
                     function(){
-                        goLink.fadeOut(options.speed)
+                        L.goLink.fadeOut(L.o.speed)
                     }
                     );
             }
 
-            //prepare to launch
-            goLink.hover(
-                function(){
-                    if(t) {
-                        clearTimeout(t);
-                    }
-                    t = setTimeout(launch, options.hoverTime);
-                },
-                function(){
-                    clearTimeout(t);
-                }
-                );//goLink.hover
 
-            var launch = function() {
-                switch(options.linkAction){
+            L.launch = function() {
+                switch(L.o.linkAction){
                     case "open":
                         window.open(this);
                         break;
@@ -74,9 +60,25 @@
                         window.open(this, '_blank');
                         break;
                     default:
-                        window.location = ahref;
+                        window.location = L.ahref;
                 }
             };
+
+
+            L.t = {};
+            //prepare to launch
+            L.goLink.hover(
+                function(){
+                    if(L.t) {
+                        clearTimeout(L.t);
+                    }
+                    L.t = setTimeout(L.launch, L.o.hoverTime);
+                },
+                function(){
+                    clearTimeout(L.t);
+                }
+                );//L.goLink.hover
+
 
         });//each
     };
